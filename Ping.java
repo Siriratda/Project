238541763f79795b40bde8eb4860ec8ad3d0e9a6
Siriratda <144195995+Siriratda@users.noreply.github.com>
2024-11-03 10:36:09 +0700

    Create for Ping

---
+++ Ping.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Ping {
+    public static void main(String[] args) throws UnknownHostException, IOException {
+    String ipAddress = "172.20.10.7";
+    InetAddress inet = InetAddress.getByName(ipAddress);
+
+    System.out.println("Sending Ping Request to " + ipAddress);
+    System.out.println(inet.isReachable(5000) ? "Host is reachable" : "Host is NOT reachable");
+
+    ipAddress = "192.168.1.111";
+    inet = InetAddress.getByName(ipAddress);
+
+    System.out.println("Sending Ping Request to " + ipAddress);
+    System.out.println(inet.isReachable(5000) ? "Host is reachable" : "Host is NOT reachable");
+}
+}
